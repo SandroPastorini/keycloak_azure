f4e5f71e92aa855f0b17be371e9a5f91803624be
Sandro Pastorini <sandropastorini@gmail.com>
2023-09-13 19:13:06 -0300

    Update customscript.sh
    added apt upgrade

--- scripts/customscript.sh
+++ scripts/customscript.sh
@@ -15,6 +15,9 @@
 
 
 sudo apt-get -y update
+sudo apt-get -y upgrade
+
+sudo apt-get -y update
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
